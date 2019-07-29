d22bbde2c694a4d89163e389c855cddd9ea3337b
72060780 <zengw@hyxt.com>
2019-07-29 17:15:12 +0800

    test add api

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const svgCaptcha = require('svg-captcha')
 const multipart = require('connect-multiparty')
-const multer  = require('multer');
+const multer = require('multer');
 const storage = multer.diskStorage({
     // //设置上传后文件路径，uploads文件夹会自动创建。
     destination: function (req, file, cb) {
@@ -17,13 +17,20 @@ const storage = multer.diskStorage({
     },
     //给上传文件重命名，获取添加后缀名
     filename: function (req, file, cb) {
-        var fileFormat = (file.originalname).split(".");
+        let fileFormat = (file.originalname).split(".");
         // console.log(file)
         // cb(null, file.fieldname + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
         cb(null, file.originalname)
     }
 });
-let uploadInfo = multer({ storage })
+let uploadInfo = multer({storage})
+let mysql = require('mysql');
+const fs = require('fs');
+let data = fs.readFileSync('./config.json')
+const databaseData = JSON.parse(data.toString())
+let connection = mysql.createConnection(databaseData);
+
+
 app.engine('html', require('ejs').renderFile)
 app.set('views', path.join(__dirname, '../public'))
 app.set('view engine', 'html')
@@ -32,21 +39,21 @@ app.use(bodyParser.json());
 app.use('/static', express.static('public/static'));
 
 const multipartMiddleware = multipart()
-const urlencodedParser = bodyParser.urlencoded({ extended: false }) // 如果前台传递的类型是Form Data类型的数据
+const urlencodedParser = bodyParser.urlencoded({extended: false}) // 如果前台传递的类型是Form Data类型的数据
 
 // 相当于拦截器，可用于全局检测token
 app.all("*", (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*"); //设置允许客户端跨域请求
     res.header("Content-Type", "application/json;charset=UTF-8"); //设置响应头数据类型
     // console.log(111)
-    if(req.method === 'POST'){
+    if (req.method === 'POST') {
         // console.log('先进我这')
         // checkToken(req, res, next, o => {
         //     // next()
         //     res.json(getJson('大哥你token呢？', 666))
         // })
         next()
-    }else{
+    } else {
         next()
     }
 })
@@ -82,6 +89,44 @@ app.get('/testdownload', (request, response, next) => {
     response.download(path)
 })
 
+// 获取数据库list
+app.get('/getlist', (request, response, next) => {
+    connection.connect();
+    let sql = 'SELECT * FROM user';
+    connection.query(sql, function (err, result) {
+        if (err) {
+            console.log('[SELECT ERROR] - ', err.message);
+            return;
+        }
+        console.log('--------------------------SELECT----------------------------');
+        console.log(result);
+        response.json({code: 200, message: 'hello world', data: result});
+        console.log('------------------------------------------------------------\n\n');
+    });
+    connection.end();
+})
+
+// 增加实例
+app.post('/add', (request, response, next) => {
+    connection.connect();
+    let {name, age, id} = request.body
+    let sql = 'INSERT INTO user(id,name,age) VALUES(?,?,?)';
+    let sqlParams = [id || 0, name || '', age || '0']
+    console.log(sqlParams)
+    connection.query(sql, sqlParams, function (err, result) {
+        if (err) {
+            console.log('[SELECT ERROR] - ', err.message);
+            next()
+            return;
+        }
+        console.log('--------------------------SELECT----------------------------');
+        console.log(result);
+        response.json({code: 200, message: 'hello world', data: ''});
+        console.log('------------------------------------------------------------\n\n');
+    });
+    connection.end();
+})
+
 // 测试验证码
 app.post('/captcha', (req, res, next) => {
     let codeConfig = {
@@ -97,8 +142,8 @@ app.post('/captcha', (req, res, next) => {
     // console.log('--------------')
     // req.session.captcha = captcha.text.toLowerCase(); //存session用于验证接口获取文字码
     let codeData = {
-        img:captcha.data,
-        msg:captcha.text.toLowerCase()
+        img: captcha.data,
+        msg: captcha.text.toLowerCase()
     }
     res.send(codeData);
 })
@@ -109,7 +154,7 @@ app.post('/upload', uploadInfo.single('file'), function (request, response, next
     console.log(request.body, request.file)
     try {
         response.json({code: 200, message: 'hello world', data: ''});
-    }catch (e) {
+    } catch (e) {
         response.json({code: 400, message: e, data: ''});
     }
 })
@@ -117,7 +162,7 @@ app.post('/upload', uploadInfo.single('file'), function (request, response, next
 // 重定向页面
 app.get('*', (request, response, next) => {
     // console.log(request.url)
-    response.writeHead(302, { 'Location': '/login' })
+    response.writeHead(302, {'Location': '/login'})
     response.end()
 })
 
@@ -127,11 +172,12 @@ app.post('*', (request, response, next) => {
     next()
 })
 
+
 const server = app.listen(8080, function () {
 
     const host = server.address().address
     const port = server.address().port
 
-    console.log("应用实例，访问地址为 http://localhost:" +  port)
+    console.log("应用实例，访问地址为 http://localhost:" + port)
 
 })
