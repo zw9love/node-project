2209f4adc373e84268b4ebe20702e913b933dfff
72060780 <zengw@hyxt.com>
2019-08-02 11:35:25 +0800

    test transaction

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 let data = fs.readFileSync('./config.json')
 const databaseData = JSON.parse(data.toString())
 let connection = mysql.createConnection(databaseData);
-
+const async = require('async');
+let {execTrans, _getNewSqlParamEntity} = require('../utils/dbHelper')
 /**
  * @api {get} /user/getList 获取用户列表
  * @apiDescription 获取用户列表
@@ -23,6 +24,7 @@ let connection = mysql.createConnection(databaseData);
  * @apiSampleRequest http://localhost:8080/user/getList
  * @apiVersion 1.0.0
  */
+
 // 获取数据库list
 router.get('/getList', (request, response, next) => {
     connection.connect();
@@ -78,5 +80,85 @@ router.post('/add', (request, response, next) => {
     });
     connection.end();
 })
+
+
+/**
+ * @api {post} /transaction 测试事务
+ * @apiDescription 测试事务
+ * @apiName /transaction
+ * @apiGroup user
+ * @apiParam {number} money=1 转钱数值
+ * @apiSuccess {json} data 返回值
+ * @apiSuccessExample {json} Success-Response:
+ {
+     "code": 200,
+     "message": "hello world",
+     "data": ""
+ }
+ * @apiSampleRequest http://localhost:8080/user/transaction
+ * @apiVersion 1.0.0
+ */
+// 增加实例
+router.post('/transaction', (request, response, next) => {
+    connection.connect();
+    let {money} = request.body
+    let sql1 = 'UPDATE  user SET money = money - ?  WHERE id = 1';
+    let sql2 = 'UPDATE  user SET money = money + ? WHERE id = 2';
+    let sqlParams = [money]
+
+    let sqlParamsEntity = [];
+    sqlParamsEntity.push(_getNewSqlParamEntity(sql1, sqlParams));
+    sqlParamsEntity.push(_getNewSqlParamEntity(sql2, sqlParams));
+
+    execTrans(sqlParamsEntity, function(err, info){
+        if(err){
+            console.error("事务执行失败");
+            console.error(err);
+        }else{
+            response.json({code: 200, message: 'hello world', data: ''});
+            console.log("done.");
+        }
+    })
+
+})
+
+
+// 分页
+// router.get('/pagination', function(req, res, next) {
+//     let current_page = 1; //默认为1
+//     let num = 9; //一页条数
+//     if (req.query.page) {
+//         current_page = parseInt(req.query.page);
+//     }
+//
+//     let last_page = current_page - 1;
+//     if (current_page <= 1) {
+//         last_page = 1;
+//     }
+//     let next_page = current_page + 1;
+//     let str = 'SELECT left(paragraph,50) as paragraph,date,id FROM notice limit ' + num + ' offset ' + num * (current_page - 1);
+//     let conn = mysql.createConnection(settings.db);
+//
+//     conn.connect();
+//     conn.query(str, function(err, rows, fields) {
+//         if (err) {
+//             req.flash('error', '数据查询有误');
+//         }
+//         if (!err) {
+//             if (!rows[0]) {
+//                 req.flash('error', '已到最后一页,请返回');
+//             }
+//             res.render('notice', {
+//                 last_page: last_page,
+//                 next_page: next_page,
+//                 current_page: current_page,
+//                 mes: rows,
+//                 error: req.flash('error').toString()
+//             });
+//
+//         }
+//     });
+//     conn.end();
+// });
 
 module.exports = router;
---
+++ src/utils/dbHelper.js
@@ -0,0 +1,81 @@
+let mysql = require('mysql');
+let async = require("async");
+let mysqlOptions = require("../../config")
+
+module.exports = {
+    execTrans,
+    _getNewSqlParamEntity
+}
+
+let pool = mysql.createPool(mysqlOptions);
+
+function execTrans(sqlparamsEntities, callback) {
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            return callback(err, null);
+        }
+        connection.beginTransaction(function (err) {
+            if (err) {
+                return callback(err, null);
+            }
+            console.log("开始执行transaction，共执行" + sqlparamsEntities.length + "条数据");
+            let funcAry = [];
+            sqlparamsEntities.forEach(function (sql_param) {
+                let temp = function (cb) {
+                    let sql = sql_param.sql;
+                    let param = sql_param.params;
+                    connection.query(sql, param, function (tErr, rows, fields) {
+                        if (tErr) {
+                            connection.rollback(function () {
+                                console.log("事务失败，" + sql_param + "，ERROR：" + tErr);
+                                throw tErr;
+                            });
+                        } else {
+                            return cb(null, 'ok');
+                        }
+                    })
+                };
+                funcAry.push(temp);
+            });
+
+            async.series(funcAry, function (err, result) {
+                console.log("transaction error: " + err);
+                if (err) {
+                    connection.rollback(function (err) {
+                        console.log("transaction error: " + err);
+                        connection.release();
+                        return callback(err, null);
+                    });
+                } else {
+                    connection.commit(function (err, info) {
+                        console.log("transaction info: " + JSON.stringify(info));
+                        if (err) {
+                            console.log("执行事务失败，" + err);
+                            connection.rollback(function (err) {
+                                console.log("transaction error: " + err);
+                                connection.release();
+                                return callback(err, null);
+                            });
+                        } else {
+                            connection.release();
+                            return callback(null, info);
+                        }
+                    })
+                }
+            })
+        });
+    });
+}
+
+function _getNewSqlParamEntity(sql, params, callback) {
+    if (callback) {
+        return callback(null, {
+            sql: sql,
+            params: params
+        });
+    }
+    return {
+        sql: sql,
+        params: params
+    };
+}
