ff5ffe298a271b607e22c7e142c0bc24832ccafc
72060780 <zengw@hyxt.com>
2019-08-12 19:30:46 +0800

    test more process

--- src/index.js
+++ src/index.js
@@ -311,3 +311,75 @@ const openDefaultBrowser = function (url) {
             exec('xdg-open', [url]);
     }
 }
+
+// -----------------------------测试多进程cluster-------------------------------
+
+// 不特意开启多进程支持的正常情况下
+
+// const seqArr = [44, 42, 43, 44]
+//
+// function fibonacci (n) {
+//     return n === 0
+//         ? 0
+//         : n === 1
+//             ? 1
+//             : fibonacci(n - 1) + fibonacci(n - 2)
+// }
+//
+// function calculate (seq, taskId) {
+//     return new Promise((resolve, reject) => {
+//         console.log(`Task ${taskId} starts calculating.`)
+//         const start = Date.now()
+//         const result = fibonacci(seq)
+//         console.log(`The result of task ${taskId} is ${result}, taking ${Date.now() - start} ms.`)
+//         return resolve(result)
+//     })
+// }
+//
+// ;(async function main () {
+//     console.time('main')
+//     const results = await Promise.all(seqArr.map(calculate))
+//     results.forEach((result, index) => console.log(`Task ${index}'s result is ${result}`))
+//     console.timeEnd('main')
+// })()
+
+// 使用cluster多进程
+// const cluster = require('cluster')
+// const numCPUs = require('os').cpus().length
+//
+// function fibonacci (n) {
+//     return n === 0
+//         ? 0
+//         : n === 1
+//             ? 1
+//             : fibonacci(n - 1) + fibonacci(n - 2)
+// }
+//
+// if (cluster.isMaster) {
+//     const seqArr = [44, 42, 43, 44]
+//     let endTaskNum = 0
+//
+//     console.time('main')
+//     console.log(`[Master]# Master starts running. pid: ${process.pid}`)
+//     for (let i = 0; i < numCPUs; i++) {
+//         const worker = cluster.fork()
+//         worker.send(seqArr[i] + '')
+//     }
+//     cluster.on('message', (worker, message, handle) => {
+//         console.log(`[Master]# Worker ${worker.id}: ${message}`)
+//         endTaskNum++
+//         if (endTaskNum === 4) {
+//             console.timeEnd('main')
+//             cluster.disconnect()
+//         }
+//     })
+//     cluster.on('exit', (worker, code, signal) => console.log(`[Master]# Worker ${worker.id} died.`))
+// } else {
+//     process.on('message', seq => {
+//         console.log(`[Worker]# starts calculating...`)
+//         const start = Date.now()
+//         const result = fibonacci(seq)
+//         console.log(`[Worker]# The result of task ${process.pid} is ${result}, taking ${Date.now() - start} ms.`)
+//         process.send('My task has ended.')
+//     })
+// }
